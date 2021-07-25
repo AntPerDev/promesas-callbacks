aeba63a7d56c5c47f47cb9c8c130fdf7cfba2739
Antonio Pérez <antperdev@gmail.com>
2021-07-25 21:37:21 +0200

    callback hell

--- src/index.js
+++ src/index.js
@@ -9,19 +9,24 @@ import './styles.css';
 // });
 
 
-const heroeId = 'capi';
+const heroeId1 = 'capi';
+const heroeId2 = 'iron';
+const heroeId2 = 'spider';
 
-buscarHeroe( heroeId , (err, heroe )=>{
+buscarHeroe( heroeId1, (err, heroe )=>{
 
-  if( err ){
-    
-    console.error( err );
-
-  } else {
-
-    console.log( heroe );
-    
-  };
+  if( err ){ return console.error( err ); }
   
+  buscarHeroe(heroeId2, (err,heroe2)=>{
+    if (err) { return console.error(err); }
+
+    buscarHeroe(heroeId3, (err, heroe2) => {
+      if (err) { return console.error(err); }
+      console.log(`Enviando a ${heroeId1.nombre}, ${heroeId2.nombre} y ${heroeId3.nombre}`);
+
+    })
+
+  })
+
 });
 
